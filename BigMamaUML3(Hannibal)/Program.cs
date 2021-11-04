57a80c072bfdb127febd66d5c878645b9cc9018d
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2021-11-04 10:45:57 +0100

    Add project files.

---
+++ BigMamaUML3(Hannibal)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigMamaUML3_Hannibal_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
