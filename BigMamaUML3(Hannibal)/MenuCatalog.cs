fd8316833adae3ec9ff3fc9025bfc8e308597413
NordicAngel <78414012+NordicAngel@users.noreply.github.com>
2021-11-08 11:27:17 +0100

    did some stuff

--- BigMamaUML3(Hannibal)/MenuCatalog.cs
+++ BigMamaUML3(Hannibal)/MenuCatalog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,61 +11,85 @@ namespace BigMamaUML3_Hannibal_
     {
         public MenuCatalog()
         {
-            _items = new List<IMenuItem>();
+            _items = new Dictionary<int, IMenuItem>();
         }
-        private List<IMenuItem> _items;
+        private Dictionary<int, IMenuItem> _items;
 
         public int Count => _items.Count;
 
         public void Add(IMenuItem aMenuItem)
         {
-            _items.Add(aMenuItem);
+            if (!_items.ContainsKey(aMenuItem.Number))
+            {
+                _items.Add(aMenuItem.Number, aMenuItem); 
+            }
+            else
+            {
+                throw new MenuItemNumberExist();
+            }
         }
 
         public IMenuItem Search(int number)
         {
-            return _items.First(x => x.Number == number);
+            if (_items.ContainsKey(number))
+            {
+                return _items[number]; 
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public void Delete(int number)
         {
-            _items.Remove(Search(number));
+            _items.Remove(number);
         }
 
         public void PrintPizzasMenu()
         {
             Console.WriteLine(string.Join(
-                "\n", from item in _items where item.Type == MenuType.Pizza select item.PrintInfo()));
+                "\n", from item in _items.Values where item.Type is MenuType.Pizza select item.PrintInfo()));
         }
 
         public void PrintBeveragesMenu()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(string.Join(
+                "\n", from item in _items.Values 
+                where item.Type is MenuType.SoftDrink or MenuType.AlcoholicDrink 
+                select item.PrintInfo()));
         }
 
         public void PrintToppingsMenu()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(string.Join(
+                "\n", from item in _items.Values
+                where item.Type is MenuType.Topping
+                select item.PrintInfo()));
         }
 
         public void Update(int number, IMenuItem theMenuItem)
         {
-            throw new NotImplementedException();
+            if (theMenuItem.Number == number)
+            {
+                Delete(number);
+                Add(theMenuItem);
+            }
         }
 
         public List<IMenuItem> FindAllVegan(MenuType type)
         {
-            throw new NotImplementedException();
+            return (from item in _items.Values where item.IsVegan select item).ToList();
         }
 
         public List<IMenuItem> FindAllOrganic(MenuType type)
         {
-            throw new NotImplementedException();
+            return (from item in _items.Values where item.IsOrganic select item).ToList();
         }
 
         public IMenuItem MostExpensiveMenuItem()
         {
-            throw new NotImplementedException();
+            return (from item in _items.Values orderby item.Price descending select item).First();
         }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BigMamaUML3_Hannibal_;
 
@@ -17,7 +18,74 @@ namespace TestProject1
             catalog.Add(new Pizza(2, "pizza", "tomato, chess, ham", 60, MenuType.Pizza, false, true, true));
 
             //assert
-            Assert.AreEqual(catalog.Count, 2);
+            Assert.AreEqual(2, catalog.Count);
+        }
+
+        [TestMethod]
+        public void MenuCatalogSearchTest()
+        {
+            //arrange
+            MenuCatalog catalog = new MenuCatalog();
+            MenuItem pizza = new Pizza(5, "pizza5", "tomato chess ham pineapple", 65, MenuType.Pizza, false, true,
+                false);
+
+            //act
+            catalog.Add(new Beverage(1, "cola", "it's cola", 30, MenuType.SoftDrink, true, false, false));
+            catalog.Add(new Pizza(2, "pizza", "tomato, chess, ham", 60, MenuType.Pizza, false, true, true));
+            catalog.Add(pizza);
+
+            //assert
+            Assert.AreSame(pizza, catalog.Search(5));
+        }
+
+        [TestMethod]
+        public void MenuCatalogRemoveTest()
+        {
+            //arrange
+            var catalog = new MenuCatalog();
+
+            //act
+            catalog.Add(new Beverage(1, "cola", "it's cola", 30, MenuType.SoftDrink, true, false, false));
+            catalog.Add(new Pizza(2, "pizza", "tomato, chess, ham", 60, MenuType.Pizza, false, true, true));
+            catalog.Delete(1);
+            catalog.Delete(2);
+
+            //assert
+            Assert.AreEqual(0, catalog.Count);
+        }
+
+        [TestMethod]
+        public void MenuCatalogMostexpensivTest()
+        {
+            //arrange
+            MenuCatalog catalog = new MenuCatalog();
+
+            //act
+            catalog.Add(new Pizza(2, "pizza", "tomato, chess, ham", 70, MenuType.Pizza, false, true, true));
+            catalog.Add(new Pizza(6, "pizza", "tomato, chess, ham", 80, MenuType.Pizza, false, true, true));
+            catalog.Add(new Pizza(4, "pizza", "tomato, chess, ham", 60, MenuType.Pizza, false, true, true));
+            catalog.Add(new Beverage(1, "cola", "it's cola", 30, MenuType.SoftDrink, true, false, false));
+            catalog.Add(new Beverage(3, "cola", "it's cola", 25, MenuType.SoftDrink, true, false, false));
+            catalog.Add(new Beverage(5, "cola", "it's cola", 41, MenuType.SoftDrink, true, false, false));
+
+            //assert
+            Assert.AreEqual(80, catalog.MostExpensiveMenuItem().Price);
+        }
+
+        [TestMethod]
+        public void MenuItemNumberExistTest()
+        {
+            //arrange
+            MenuCatalog catalog = new MenuCatalog();
+
+            //act
+            catalog.Add(new Beverage(1,"cola","it's fucking cola",30,MenuType.SoftDrink,true,false,false));
+
+            Action AddWithSameNumber = () =>
+                catalog.Add(new Pizza(1, "pizza", "IDK", 60, MenuType.Pizza, false, false, true));
+
+            //assert
+            Assert.ThrowsException<MenuItemNumberExist>(AddWithSameNumber);
         }
     }
 }
